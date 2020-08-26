4fd3e419cf6da91fe529a9104f763d6bc707ebfe
Brian Pereira <pereirabrian@gmail.com>
2020-08-26 23:48:26 +0530

    First commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is my Git repository for C programs.
---
+++ gitproj.c
@@ -0,0 +1,13 @@
+/*
+   Author: Brian Pereira and Odelia Pereira
+   Date: 26th August, 2020
+   Purpose: To learn C programming and use GitHub
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  printf("Program ran successfully\n");
+  return 0;
+}
