003b3a4a37e0b11f6739dcdd02e688e6cdefeeed
Odelia Pereira <pereiraodelia@gmail.com>
2020-08-27 23:04:26 +0530

    Added the o_mathlib.h library

---
+++ o_mathlib.h
@@ -0,0 +1,16 @@
+/* 
+  Author: Odelia Pereira
+  Date: 27th August 2020
+  Purpose: Odelia's Math library.
+*/
+
+/* Last updated: 27th August 2020
+   Added function: add
+*/
+
+
+int add(int x, int y)
+{
+  return x + y;
+}
+
